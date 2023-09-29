4d17218234247c98f6ca65fe374607cbf3822a17
hackerdud3 <nandikantivinay35@gmail.com>
2023-09-28 20:49:11 -0700

    Spring security

--- src/main/java/com/pulseevents/pulse/security/SecurityConfig.java
+++ src/main/java/com/pulseevents/pulse/security/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.pulseevents.pulse.security;
 
+import com.pulseevents.pulse.security.jwt.AuthEntryPointJwt;
+import com.pulseevents.pulse.security.jwt.AuthTokenFilter;
 import com.pulseevents.pulse.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    public AuthTokenFilter aut
 
 }
--- src/main/java/com/pulseevents/pulse/service/UserDetailsServiceImpl.java
+++ src/main/java/com/pulseevents/pulse/service/UserDetailsServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -16,6 +17,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     UserRepo userRepo;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username " + username));
         return UserDetailsImpl.build(user);
