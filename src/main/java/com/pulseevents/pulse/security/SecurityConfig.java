6f6f78db565415b0ce78d497ee092dd7ab81b253
hackerdud3 <nandikantivinay35@gmail.com>
2023-10-02 17:49:13 -0700

    Added JWT Authentication and role based Authorization

--- src/main/java/com/pulseevents/pulse/security/SecurityConfig.java
+++ src/main/java/com/pulseevents/pulse/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll().requestMatchers("/**").permitAll().anyRequest().authenticated());
+                        .requestMatchers("/api/auth/**").permitAll().requestMatchers("/api/test/**").permitAll().anyRequest().authenticated());
 
         http.authenticationProvider(authenticationProvider());
 
