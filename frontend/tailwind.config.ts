7f09ed882df5c4ccc7c09a14a88ed267745cbb12
hackerdud3 <nandikantivinay35@gmail.com>
2023-10-07 03:52:20 -0700

    login and signup

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontSize: {
+        h1: "1.25rem",
+        h2: "1.125rem",
+        h3: "1rem",
+        h4: "0.875rem",
+        p: "1rem",
+      },
+      lineHeight: {
+        h1: "1.75rem",
+        h2: "1.75rem",
+        h3: "1.5rem",
+        h4: "1.25rem",
+        p: "1.5rem",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
