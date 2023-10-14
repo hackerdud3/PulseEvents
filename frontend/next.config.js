9bb3d85cd7dfc2ce679d01221bd38e42276858c8
hackerdud3 <nandikantivinay35@gmail.com>
2023-10-13 18:46:55 -0700

    Front end production

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ["style-loader", "css-loader", "postcss-loader"],
+    });
+    return config;
+  },
+};
