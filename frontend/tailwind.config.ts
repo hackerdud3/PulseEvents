ba4041a53b298fafa49ba95669325097fb083ec4
hackerdud3 <nandikantivinay35@gmail.com>
2024-06-16 11:04:02 -0700

    Updated package.json and next js configs

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss';
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
+import { nextui, colors } from '@nextui-org/react';
+
+module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}'
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      colors: {
+        primary: '#5669FF',
+        secondary: '#3D56F0'
       }
     }
   },
-  plugins: []
+  plugins: [nextui()]
 };
-export default config;
