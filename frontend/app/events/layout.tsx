d38e7ce4416151a0ff728a0de13e2dcdbb17161e
hackerdud3 <nandikantivinay35@gmail.com>
2024-06-16 11:19:15 -0700

    Removed events/page.tsx and event.css -Updated events/layout.tsx

--- frontend/app/events/layout.tsx
+++ frontend/app/events/layout.tsx
@@ -1,13 +1,13 @@
-import { Disclosure } from '@headlessui/react';
-
-export default function RootLayout({
+export default function EventsLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-7xl relative mx-auto justify-center items-center px-2 sm:px-6 lg:px-8">
-      {children}
-    </div>
+    <>
+      <main className="flex h-full w-full max-w-[1024px] mx-auto items-center flex-col">
+        {children}
+      </main>
+    </>
   );
 }
