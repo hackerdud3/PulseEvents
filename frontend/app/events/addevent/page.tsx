4fe675243a740a04a83a3c2b03af6c354c7bf6a5
hackerdud3 <nandikantivinay35@gmail.com>
2024-06-16 10:11:55 -0700

    Refactor events/layout and events/page

--- frontend/app/events/addevent/page.tsx
+++ frontend/app/events/addevent/page.tsx
@@ -1,181 +1,24 @@
-'use client';
+import React from 'react';
+import EventForm from '../../../components/forms/EventForm';
+import EventCard from '../../../components/EventCard';
 
-import { Button } from '@mui/material';
-import React, { FormEvent, useState } from 'react';
-import axios from 'axios';
-import { useAuth } from '@/Contexts/Auth';
-
-const AddEvent = () => {
-  const [image, setImage] = useState<File | null>(null);
-  const { user } = useAuth();
-
-  const [formData, setFormData] = useState({
-    event_name: '',
-    venue: '',
-    num_attending: '',
-    attending: true
+async function fetchCategories() {
+  const response = await fetch('http://localhost:8080/categories', {
+    cache: 'no-store' // Ensure fresh data on each request
   });
+  if (!response.ok) {
+    throw new Error('Failed to fetch categories');
+  }
+  return response.json();
+}
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    console.log(formData);
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      setImage(file);
-    }
-  };
-
-  const handleSubmit = async (event: FormEvent) => {
-    const token = user.token;
-    event.preventDefault();
-    if (!image) {
-      console.error('No image selected');
-      return;
-    }
-
-    const formDataToSend = new FormData();
-    formDataToSend.append('eventImage', image); // Append the file
-    formDataToSend.append('eventData', JSON.stringify(formData));
-    formDataToSend.append('token', token);
-
-    try {
-      const response = await axios.post(
-        'http://localhost:8080/addevent',
-        formDataToSend,
-        {
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'multipart/form-data'
-          }
-        }
-      );
-
-      if (response.status === 200) {
-        console.log('Event added successfully!');
-      } else {
-        console.error('Error adding event:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
+const page = async () => {
+  const categories = await fetchCategories();
   return (
-    <div className="flex justify-center items-center w-full">
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col w-96 mt-8 gap-8"
-        encType="multipart/form-data"
-      >
-        <div className="flex w-full justify-center items-center gap-8">
-          <div className="rounded-md shadow-sm w-full ">
-            <input
-              type="text"
-              name="event_name"
-              id="event_name"
-              value={formData.event_name}
-              onChange={handleChange}
-              className="h-10 block w-full rounded-md border border-gray-200 px-4 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              placeholder="Event Name"
-              spellCheck={false}
-            />
-          </div>
-
-          <div className="rounded-md shadow-sm w-full">
-            <input
-              type="text"
-              name="venue"
-              id="venue"
-              value={formData.venue}
-              onChange={handleChange}
-              className="h-10 block w-full rounded-md border border-gray-200 px-4 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              placeholder="Venue"
-              spellCheck={false}
-            />
-          </div>
-        </div>
-
-        <div className="flex justify-center items-center gap-8 w-full">
-          {/* <div className="rounded-md shadow-sm w-full">
-            <input
-              type="date"
-              name="date"
-              id="date"
-              value={formData.date}
-              onChange={handleChange}
-              className="h-10 block w-full rounded-md border border-gray-200 px-4 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              placeholder="Date"
-              spellCheck={false}
-            />
-          </div> */}
-        </div>
-
-        <div className="flex w-full justify-center items-center gap-8">
-          <div className="rounded-md shadow-sm w-full ">
-            <input
-              type="number"
-              name="num_attending"
-              id="num_attending"
-              value={formData.num_attending}
-              onChange={handleChange}
-              className="h-10 block w-full rounded-md border border-gray-200 px-4 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              placeholder="Number of Participants"
-              spellCheck={false}
-            />
-          </div>
-
-          <div className="rounded-md shadow-sm w-full">
-            <select
-              name="attending"
-              id="attending"
-              value={formData.attending.toString()}
-              onChange={handleChange}
-              className="h-10 block w-full rounded-md border border-gray-200 px-4 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            >
-              <option value="true">Yes</option>
-              <option value="false">No</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="rounded-md shadow-sm w-full">
-          <label
-            htmlFor="eventImage"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Event Image
-          </label>
-          <input
-            type="file"
-            accept="image/*"
-            name="eventImage"
-            id="eventImage"
-            onChange={handleFileChange}
-            className="sr-only"
-          />
-          <label
-            htmlFor="eventImage"
-            className="h-10 block w-full rounded-md border border-gray-200 pl-4 pr-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm cursor-pointer"
-          >
-            Choose an image
-          </label>
-        </div>
-
-        <div className="flex  w-full items-center justify-end">
-          <Button type="submit">Create Event</Button>
-        </div>
-      </form>
+    <div className="w-full flex justify-center items-start bg-gradient-to-r rounded-2xl">
+      <EventForm categoryList={categories} />
     </div>
   );
 };
 
-export default AddEvent;
+export default page;
--- frontend/app/events/page.tsx
+++ frontend/app/events/page.tsx
@@ -1,145 +1,48 @@
-'use client';
-import React, { useEffect, useState, useRef, lazy } from 'react';
-import AddRoundedIcon from '@mui/icons-material/AddRounded';
-import Image from 'next/image';
-import {
-  Button,
-  CardContent,
-  CardMedia,
-  IconButton,
-  Typography,
-  styled
-} from '@mui/material';
-import axios from 'axios';
-import { useAuth } from '../../Contexts/Auth';
-
+import React from 'react';
 import Link from 'next/link';
-
-import { ButtonProps } from '@tremor/react';
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
-import './event.css';
-import EveCard from '../components/card';
-import useFetchEvents from '@/Hooks/fetchEvents';
-import { Disclosure } from '@headlessui/react';
+import { Binary } from 'bson';
+import EventCard from '../../components/EventCard';
+import LandingImage from '../../components/LandingImage';
 
 type Props = {};
 
-async function fetchGoogleEvents() {
-  const response = await axios.get(
-    'https://www.googleapis.com/calendar/v3/calendars/calendarId/events/eventId'
-  );
-  return response.data;
+interface Event {
+  eid: string;
+  eventName: string;
+  description: string;
+  venue: string;
+  startDate: string;
+  endDate: string;
+  createdBy: string;
+  interestedCount: number;
+  eventImage: Binary | null;
+  categories: string[];
 }
 
-const AddButton = styled(Button)<ButtonProps>(({ theme }) => ({
-  color: 'white',
-  padding: '6px 10px',
-  backgroundColor: '#3d48da',
-  '&:hover': {
-    backgroundColor: '#434ff0'
+async function fetchEvents() {
+  const response = await fetch('http://localhost:8080/events', {
+    cache: 'no-cache'
+  });
+  if (!response.ok) {
+    throw new Error('Failed to fetch events');
   }
-}));
+  return response.json() as Promise<Event[]>;
+}
 
-function EventsPage({}: Props) {
-  const [attending, isAttending] = useState(false);
-  const containerRef = useRef<any>(null);
-  const [googleEvents, setGoogleEvents] = useState<any>(null);
-  const storedUserData = localStorage.getItem('userData');
-
-  let token = null;
-
-  if (storedUserData) {
-    const userData = JSON.parse(storedUserData);
-    token = userData.token;
-  }
-  const events = useFetchEvents({ token });
-
-  const toggleInterested = () => {
-    isAttending(!attending);
-  };
-
-  const handleArrowButtonClicked = (direction: string) => {
-    const container = containerRef.current;
-    const scrollAmount = container.offsetWidth + 10; // Width of the container
-
-    if (container) {
-      if (direction === 'left') {
-        container.scrollLeft -= scrollAmount;
-      } else if (direction === 'right') {
-        container.scrollLeft += scrollAmount;
-      }
-    }
-  };
+async function EventsPage({}: Props) {
+  const events = await fetchEvents();
+  console.log(events);
 
   return (
-    <div className="w-full flex flex-col item-center justify-center mt-12 ">
-      <div>
-        <Image
-          src="/6291563.jpeg"
-          alt="pulse wallpaper"
-          className="w-full h-[20rem] object-cover object-center"
-          width={1000}
-          height={5}
-          loading="lazy"
-        />
+    <div className="flex flex-col item-center justify-center w-full p-2">
+      <div className="w-full">
+        <LandingImage />
       </div>
+      <h1 className=" font-semibold text-4xl p-4">Events</h1>
 
-      <div className="flex flex-col w-full">
-        {/* <Eventcard key={item.eid} item={item} /> */}
-
-        <div className="w-full flex items-center justify-center my-8 ">
-          <div className="flex w-full items-center justify-between">
-            <h1 className="text-lg sm:text-2xl font-bold">Upcoming Events</h1>
-
-            <Link href="events/addevent">
-              <AddButton className="bg-black" startIcon={<AddRoundedIcon />}>
-                Add Event
-              </AddButton>
-            </Link>
-          </div>
-        </div>
-
-        <div className="relative">
-          <div
-            ref={containerRef}
-            id="scroll-container"
-            className="overflow-hidden whitespace-nowrap relative"
-          >
-            <div
-              id="scroll-content"
-              className=" transition left-transition right-transition duration-1000 ease overscroll-contain grid-flow-col scroll-smooth space-x-4"
-            >
-              {events?.map((item, index) => (
-                <EveCard item={item} key={index} />
-              ))}
-            </div>
-          </div>
-          <IconButton
-            id="left-button"
-            className="bg-gray-200"
-            sx={{
-              position: 'absolute',
-              top: '120px',
-              left: '-20px'
-            }}
-            onClick={() => handleArrowButtonClicked('left')}
-          >
-            <ArrowBackIosNewRoundedIcon />
-          </IconButton>
-
-          <IconButton
-            id="right-buttom"
-            className="bg-gray-200"
-            sx={{
-              position: 'absolute',
-              top: '120px',
-              right: '-20px'
-            }}
-            onClick={() => handleArrowButtonClicked('right')}
-          >
-            <ArrowForwardIosRoundedIcon />
-          </IconButton>
+      <div className=" flex items-center justify-center w-full">
+        <div className="flex flex-col items-center justify-start min-w-full gap-4 px-4">
+          {events?.map((item) => <EventCard key={item?.eid} event={item} />)}
         </div>
       </div>
     </div>
